09c2972b18dd2b305707bec30aeb1554b777544f
imokech <choobeh77@gmail.com>
2023-03-09 13:08:04 +0330

    basic structure

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# wp-content--section-update-schedule
+# wp-content-section-update-schedule
 Content section update schedule
+
+This plugin provides to you create a schedule for updating your posts.
+
+## Features
+
+- [ ] Schedule by day
+- [ ] Schedule by time
+- [ ] Schedule by date
+- [ ] ...
+
+
---
+++ wp-csus.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Content Logger
+ * Description: Logs changes to post content.
+ * Version:     0.1
+ * Plugin URI:  https://mokech.ir
+ * Author:      Mohammad Keshavarz
+ * Author URI:  https://mokech.ir
+ * Text Domain: wcsus
+ * Domain Path: /languages/
+ * Requires at least: 5.6
+ * Requires PHP: 7.4
+ */
